0e8bf9d7ab3d78eed69eb847f5d297f34163c9e3
Daniel Nau <danielnau@Daniels-MacBook-Pro.local>
2021-09-18 12:48:00 -0500

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='content'>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default About
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import Home from './Home'
+import About from './About'
 const Navigation = () => {
     return (
-        <>
-            
-        </>
+        <Router>
+            <div className="sidebar">
+                <Link className="active" to='/'>Home</Link>
+                <Link className="active" to='/About'>About</Link>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/About" exact component={About} />
+        </Switch>
+        </ Router>
     )
 }
 
